a302d1eacedd96455eaaa0530dfe72546b2e804c
NastasjaBakovic1999 <nastasjabakovic199@gmail.com>
2022-04-23 22:00:29 +0200

    added summary doc in class Deliverer

--- DeliveryServiceDomain/Deliverer.cs
+++ DeliveryServiceDomain/Deliverer.cs
@@ -6,15 +6,45 @@ using System.Threading.Tasks;
 
 namespace DeliveryServiceDomain
 {
+    /// <summary>
+    ///     Klasa koja predstavlja dostavljaca
+    /// </summary>
+    /// <remarks>
+    ///    <para>
+    ///         U kontekstu datog softverskog sistema <br />
+    ///         ova klasa predstavlja isporucioca paketa
+    ///     </para>
+    ///     <para>
+    ///         Nasledjuje i prosiruje klasu Person.
+    ///     </para>
+    /// </remarks>
     public class Deliverer : Person
     {
+        /// <value> 
+        ///     Datum zaposlenja dostavljaca
+        /// </value>
         public DateTime DateOfEmployment { get; set; }
 
+        /// <summary>
+        ///     Funkcija koja vraca vrednost atributa DateOfEmployment
+        /// </summary>
+        /// <returns>Datum zaposlenja dostavljaca</returns>
         public DateTime GetDateOfEmployment()
         {
             return DateOfEmployment;
         }
 
+        /// <summary>
+        ///     Funkcija koja postavlja vrednost atributa DateOfEmployment
+        ///     <list type="bullet">
+        ///         <item>
+        ///             <term>Ukoliko je prosledjeni datum ili vreme u buducnosti</term>
+        ///             <description>Funkcija baca <see cref="ArgumentOutOfRangeException"/> gresku.</description>
+        ///         </item>
+        ///     </list>
+        /// </summary>
+        /// <param name="doe"></param>
+        /// <exception cref="ArgumentOutOfRangeException"></exception>
         public void SetDateOfEmployment(DateTime doe)
         {
             if(doe > DateTime.Now)
