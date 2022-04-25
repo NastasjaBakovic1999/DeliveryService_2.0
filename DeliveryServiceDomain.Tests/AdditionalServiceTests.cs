c0e9923f985d25af5d0ca67f1bf5c3178ee5de7c
NastasjaBakovic1999 <nastasjabakovic199@gmail.com>
2022-04-25 17:45:03 +0200

    added xunit testing for class AdditionalService

---
+++ DeliveryServiceDomain.Tests/AdditionalServiceTests.cs
@@ -0,0 +1,112 @@
+﻿using FakeItEasy;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Xunit;
+
+namespace DeliveryServiceDomain.Tests
+{
+    public class AdditionalServiceTests
+    {
+        private readonly AdditionalService _sut;
+        public AdditionalServiceTests()
+        {
+            _sut = new AdditionalService();
+        }
+
+        [Theory]
+        [InlineData(1,1)]
+        [InlineData(10,10)]
+        [InlineData(200,200)]
+        public void SetAdditionalServiceIdPositiveIntTheory(int expected, int id)
+        {
+            _sut.SetAddiitonalServiceId(id);
+            Assert.Equal(expected, _sut.GetAdditionalServiceId());
+        }
+
+        [Theory]
+        [InlineData(-1)]
+        [InlineData(-10)]
+        [InlineData(-199)]
+        public void SetAdditionalServiceIdNegativeIntTheory(int id)
+        {
+            Assert.Throws<ArgumentException>(() => _sut.SetAddiitonalServiceId(id));
+        }
+
+        [Theory]
+        [InlineData("SMS obavestenje")]
+        [InlineData("Osiguranje vrednosti")]
+        [InlineData("Dodatno pakovanje")]
+        public void SetAdditionalServiceNameCorrectNameTheory(string name)
+        {
+            _sut.SetAdditionalServiceName(name);
+            Assert.Equal(name, _sut.GetAdditionalServiceName());
+        }
+
+        [Fact]
+        public void SetAdditionalServiceNameNullFact()
+        {
+            Assert.Throws<ArgumentNullException>(() => _sut.SetAdditionalServiceName(null));
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData("   ")]
+        [InlineData("        ")]
+        public void SetAdditionalServiceNameNullTheory(string name)
+        {
+            Assert.Throws<ArgumentException>(() => _sut.SetAdditionalServiceName(name));
+        }
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(10)]
+        [InlineData(159)]
+        public void SetAdditionalServicePricePositiveAmountTheory(double price)
+        {
+            _sut.SetAdditionalServicePrice(price);
+            Assert.Equal(price, _sut.GetAdditionalServicePrice());
+        }
+
+        [Fact]
+        public void SetAdditionalServicePriceNegativeAmountFact()
+        {
+            Assert.Throws<ArgumentException>(() => _sut.SetAdditionalServicePrice(-10));
+        }
+
+        [Fact]
+        public void GetShipmentsNullFact()
+        {
+            _sut.Shipments = null;
+            Assert.Throws<NullReferenceException>(() => _sut.GetShipments());
+        }
+
+        [Fact]
+        public void GetShipmentsEmptyListFact()
+        {
+            _sut.Shipments = new List<AdditionalServiceShipment>();
+            Assert.Throws<Exception>(() => _sut.GetShipments());
+        }
+
+        [Fact]
+        public void GetShipmentsFact()
+        {
+            _sut.Shipments = (List<AdditionalServiceShipment>)A.CollectionOfFake<AdditionalServiceShipment>(10);
+            Assert.Equal(10, _sut.GetShipments().Count);
+        }
+
+        [Fact]
+        public void SetShipmentsNullFact()
+        {
+            Assert.Throws<ArgumentNullException>(() => _sut.SetShipments(null));
+        }
+
+        [Fact]
+        public void SetShipmentsEmptyListFact()
+        {
+            var shipments = (List<AdditionalServiceShipment>)A.CollectionOfFake<AdditionalServiceShipment>(0);
+            Assert.Throws<ArgumentException>(() => _sut.SetShipments(shipments));
+        }
+
+    }
+}
