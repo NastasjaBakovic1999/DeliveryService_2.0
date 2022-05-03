21562073462d906306b7c46eb45fe0f7ba06da83
NastasjaBakovic1999 <nastasjabakovic199@gmail.com>
2022-05-03 22:01:41 +0200

    implemented services for deliverer, customer and person

---
+++ DeliveryServiceApp/Services/Implementation/ServiceDeliverer.cs
@@ -0,0 +1,28 @@
+﻿using DeliveryServiceApp.Services.Interfaces;
+using DeliveryServiceData.UnitOfWork;
+using DeliveryServiceDomain;
+using System;
+using System.Collections.Generic;
+
+namespace DeliveryServiceApp.Services.Implementation
+{
+    public class ServiceDeliverer : IServiceDeliverer
+    {
+        private readonly IPersonUnitOfWork unitOfWork;
+
+        public ServiceDeliverer(IPersonUnitOfWork unitOfWork)
+        {
+            this.unitOfWork = unitOfWork;
+        }
+
+        public Deliverer FindByID(int id, params int[] ids)
+        {
+            return unitOfWork.Deliverer.FindByID(id, ids);
+        }
+
+        public List<Deliverer> GetAll()
+        {
+            return unitOfWork.Deliverer.GetAll();
+        }
+    }
+}
