213e21e8530ade7b5e76bc5aec847c3d581d99e9
Nastasja Baković <nastasjabakovic199@gmail.com>
2022-05-03 23:40:43 +0200

    added inMemory unit of works and test project for repos

---
+++ DeliveryServiceData/UnitOfWork/Implementation/InMemoryUnitOfWork.cs
@@ -0,0 +1,34 @@
+﻿using DeliveryServiceData.Implementation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeliveryServiceData.UnitOfWork.Implementation
+{
+    public class InMemoryUnitOfWork : IUnitOfWork
+    {
+        public InMemoryUnitOfWork()
+        {
+
+        }
+
+        public IRepositoryAdditionalService AdditionalService { get; set; } = new InMemoryRepositoryAdditonalService();
+        public IRepositoryAdditionalServiceShipment AdditionalServiceShipment { get; set; } = new InMemoryRepositoryAdditonalServiceShipment();
+        public IRepositoryShipment Shipment { get; set; } = new InMemoryRepositoryShipment();
+        public IRepositoryStatus Status { get; set; } = new InMemoryRepositoryStatus();
+        public IRepositoryStatusShipment StatusShipment { get; set; } = new InMemoryRepositoryStatusShipment();
+        public IRepositoryShipmentWeight ShipmentWeight { get; set; } = new InMemoryRepositoryShipmentWeight();
+
+        public void Commit()
+        {
+            
+        }
+
+        public void Dispose()
+        {
+            
+        }
+    }
+}
